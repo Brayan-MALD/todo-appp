4e53528b0357321cd4173f2130d59ec82d53aaff
Gabriel Agamez <agamezg30@gmail.com>
2025-07-02 13:42:37 -0500

    feat: componente Todos.jsx con fetch inicial usando useEffect

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from 'react';
+
 export default function Todos() {
-  return <h1>Todos</h1>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Lista de Todos</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
