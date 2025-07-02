30c4eab1fb29ddef9407ccf4630fb7a23d2ea8ed
Gabriel Agamez <agamezg30@gmail.com>
2025-07-02 13:49:43 -0500

    feat: formulario para crear nuevos todos con validación básica

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 export default function Todos() {
   const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
@@ -11,9 +12,35 @@ export default function Todos() {
       });
   }, []);
 
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+
+    if (newTodo.trim() === '') return alert('El campo no puede estar vacío');
+
+    const nuevo = {
+      id: Date.now(),
+      title: newTodo,
+      completed: false
+    };
+
+    setTodos([...todos, nuevo]);
+    setNewTodo('');
+  };
+
   return (
     <div>
       <h2>Lista de Todos</h2>
+
+      <form onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          value={newTodo}
+          placeholder="Nuevo todo..."
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button type="submit">Agregar</button>
+      </form>
+
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
